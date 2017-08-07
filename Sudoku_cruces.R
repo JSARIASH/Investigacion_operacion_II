b43b8ee84a983410d79aef603396eb68d4ee777e
Sebastián Arias <sebasariash@gmail.com>
2017-08-07 08:30:05 -0500

    Update Sudoku_cruces.R

--- Sudoku_cruces.R
+++ Sudoku_cruces.R
@@ -1,10 +1,10 @@
 Sudoku_cruces<-function(Padres_1,Padres_2,solucion_base)
 {
   #funcion que entrega que recibe los padres y entrega los los hijos
-  temp_Padres_1<-solucion_base
-  temp_Padres_2<-solucion_base
-  temp_Padres_1[temp_Padres_1==0]<-Padres_1 # Convertir el Padre 1 en posiciones de 81
-  temp_Padres_2[temp_Padres_2==0]<-Padres_2 # Convertir el Padre 2 en posiciones de 81
+  temp_Padres_1 <- solucion_base
+  temp_Padres_2 <- solucion_base
+  temp_Padres_1[temp_Padres_1==0] <- Padres_1 # Convertir el Padre 1 en posiciones de 81
+  temp_Padres_2[temp_Padres_2==0] <- Padres_2 # Convertir el Padre 2 en posiciones de 81
   
   punto_corte<-round(runif(1,1,8),0)*9 # encontrar el punto aletorio donde hacer el corte
   Primercorte<-c(1,punto_corte) #hacer el primer corte
