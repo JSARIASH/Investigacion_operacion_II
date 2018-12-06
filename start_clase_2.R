53c6a5a5e2431b087973c4a9ba2fdfd227badcf3
SebastianArias <sebasariash@gmail.com>
2018-12-06 17:02:05 -0500

    Se ordenó algunos archivos

--- start_clase_2.R
+++
@@ -1,6 +0,0 @@
-source("ui_2.R")
-source("server_2.R")
-runApp(list(
-  ui = pg,
-  server = f
-))
--- start_clase_7.R
+++
@@ -1,7 +0,0 @@
-library(shiny)
-source("ui_clase_7.R")
-source("server_clase_7.R")
-runApp(list(
-  ui = pg,
-  server = f
-))
